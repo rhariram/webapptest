55a8f7244b6bbd42132aa02bb41d251ed2c1eb2c
rhariram <rekha.hariram@gm.com>
2019-05-29 10:59:03 -0400

    Update PhoneBook.java

--- src/com/proquest/interview/phonebook/PhoneBook.java
+++ src/com/proquest/interview/phonebook/PhoneBook.java
@@ -1,6 +1,10 @@
 package com.proquest.interview.phonebook;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface PhoneBook {
-	public Person findPerson(String firstName, String lastName);
-	public void addPerson(Person newPerson);
+	public List<Person> findPerson(String firstName, String lastName);
+	public void addPerson(Person newPerson) throws Exception;
+	public Collection<Person> getPeople();
 }
