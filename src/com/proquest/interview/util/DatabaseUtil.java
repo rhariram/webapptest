04126e8c4f0150c671623966f0eb78df36b7e23f
rhariram <rekha.hariram@gm.com>
2019-05-29 11:00:29 -0400

    Update DatabaseUtil.java

--- src/com/proquest/interview/util/DatabaseUtil.java
+++ src/com/proquest/interview/util/DatabaseUtil.java
@@ -2,8 +2,14 @@ package com.proquest.interview.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.proquest.interview.phonebook.Person;
 
 /**
  * This class is just a utility class, you should not have to change anything here
@@ -27,5 +33,48 @@ public class DatabaseUtil {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.hsqldb.jdbcDriver");
 		return DriverManager.getConnection("jdbc:hsqldb:mem", "sa", "");
+	}
+	
+	public static List<Person>getPeople() throws Exception{
+		Connection cn = null;
+		try {
+			cn = getConnection();
+			Statement stmt = cn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT NAME, PHONENUMBER, ADDRESS FROM PHONEBOOK");;
+			
+			List<Person> result = new ArrayList<Person>();
+			while(rs.next()){
+				result.add(new Person( rs.getString("NAME"),rs.getString("PHONENUMBER"),rs.getString("ADDRESS")));
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(cn!=null){
+				cn.close();
+			}
+		}
+	}
+	
+	public static void addPerson(Person person) throws Exception{
+		Connection cn = null;
+		try {
+			cn = getConnection();
+			PreparedStatement stmt = cn.prepareStatement("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES(?,?,?)");
+			//these are not handling nulls - need to handle null
+			stmt.setString(1, person.getName());
+			stmt.setString(2, person.getPhoneNumber());
+			stmt.setString(3, person.getAddress());
+			stmt.executeUpdate();
+			cn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(cn!=null) {
+				cn.close();
+			}
+		}
 	}
 }
