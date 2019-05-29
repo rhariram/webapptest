7e8e6f52cda6b3db49ec4e1520d7616289e9c056
rhariram <rekha.hariram@gm.com>
2019-05-29 10:58:24 -0400

    Update Person.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -4,4 +4,34 @@ public class Person {
 	public String name;
 	public String phoneNumber;
 	public String address;
+
+	public Person(String name, String phoneNumber, String address) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString(){
+		return new StringBuffer("Name:").append(this.name).append(System.lineSeparator())
+				.append("Phone Number:").append(this.phoneNumber).append(System.lineSeparator())
+				.append("Address:").append(this.address).toString();
+	}
 }
